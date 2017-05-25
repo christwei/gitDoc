938d9e41e023266576a1daa31f1173991cbb2d8f
tangwei <495324610@qq.com>
2017-05-25 23:09:18 +0800

    5/25晚更新

---
+++ 课件/环宇/java编程基础/code/A2/ReverseString.java
@@ -0,0 +1,18 @@
+﻿
+
+public class ReverseString {
+    
+    
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+         String text = "abcde";// 获取用户输入
+         StringBuilder sbuilder = new StringBuilder(text);// 创建构建器包含用户输入
+         StringBuilder reverse = sbuilder.reverse();// 调用反转方法获取反转后的构建器
+         String reverseText = reverse.toString();// 从构建器获取反转的字符串
+         System.out.println(reverseText);
+    }
+    
+    
+}
