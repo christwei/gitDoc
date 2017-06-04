ed702977a0637592a53a6e45a17b2ed3304a1354
tangwei <495324610@qq.com>
2017-06-04 23:06:32 +0800

    6/4更新

---
+++ 课件/环宇/code/A3/BeanCopyTest.java
@@ -0,0 +1,68 @@
+﻿package com.mingrisoft.beanutils;
+
+import java.lang.reflect.InvocationTargetException;
+
+import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.beanutils.PropertyUtils;
+import java.util.HashMap;
+import java.util.Map;
+
+class Employee {
+    
+    private String name;
+    private String[] phoneNumber = new String[10];
+    private Map<String, String> address = new HashMap<String, String>();
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String[] getPhoneNumber() {
+        return phoneNumber;
+    }
+    
+    public void setPhoneNumber(String[] phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    
+    public Map<String, String> getAddress() {
+        return address;
+    }
+    
+    public void setAddress(Map<String, String> address) {
+        this.address = address;
+    }
+}
+
+public class BeanCopyTest {
+    public static void main(String[] args) {
+        Employee employee1 = new Employee();// 声明Employee变量
+        Employee employee2 = new Employee();// 声明Employee变量
+        try {
+            // 为employee1赋值
+            PropertyUtils.setSimpleProperty(employee1, "name", "明日科技");
+            PropertyUtils.setIndexedProperty(employee1, "phoneNumber", 0, "1234567");
+            PropertyUtils.setMappedProperty(employee1, "address", "home", "中国");
+            BeanUtils.copyProperties(employee2, employee1);// 将employee1复制到employee2
+            // 获得employee2的属性值
+            String name = (String) PropertyUtils.getSimpleProperty(employee2, "name");
+            String phoneNumber = (String) PropertyUtils.getIndexedProperty(employee2, "phoneNumber", 0);
+            String address = (String) PropertyUtils.getMappedProperty(employee2, "address", "home");
+            // 输出employee2的属性值
+            System.out.println("复制属性值之后：");
+            System.out.println("name属性：" + name);
+            System.out.println("phoneNumber属性的第一个值：" + phoneNumber);
+            System.out.println("address属性home键所对应的值：" + address);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ 课件/环宇/code/A3/SerializationUtilsTest.java
@@ -0,0 +1,71 @@
+﻿package com.mingrisoft.lang;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.Arrays;
+
+import org.apache.commons.lang.SerializationUtils;
+
+import java.io.Serializable;
+
+class Student implements Serializable {
+    
+    private static final long serialVersionUID = -8396517822004869094L;
+    private int id;
+    private String name;
+    
+    public int getId() {
+        return id;
+    }
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    @Override
+    public String toString() {
+        return "学生id：" + id + "，学生姓名：" + name;
+    }
+}
+
+
+public class SerializationUtilsTest {
+    public static void main(String[] args) {
+        Student student = new Student();// 创建student对象
+        student.setId(10);// 初始化id属性
+        student.setName("明日科技");// 初始化name属性
+        System.out.println("将student对象序列化成byte数组");
+        byte[] studentByte = SerializationUtils.serialize(student);// 将对象转换成byte数组
+        System.out.println("输出序列化数组：");
+        System.out.println(Arrays.toString(studentByte));// 输出byte数组
+        System.out.println("将student对象序列化到本地文件");
+        FileOutputStream out = null;// 创建文件输出流对象
+        try {
+            out = new FileOutputStream(new File("d:\\student.txt").getAbsoluteFile());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        SerializationUtils.serialize(student, out);// 将对象写入到student.txt文件
+        System.out.println("文件生成成功！");
+        System.out.println("从本地文件反序列化student对象");
+        FileInputStream in = null;// 创建文件输入流对象
+        try {
+            in = new FileInputStream(new File("d:\\student.txt").getAbsoluteFile());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        Student newStudent = (Student) SerializationUtils.deserialize(in);// 读入对象
+        System.out.println("查看student对象的属性");
+        System.out.println(newStudent);
+    }
+}
